96a9117dfcea50a669f66a0a530f4c2d82f13536
leticiaavacamathe <leticia_a.m@hotmail.com>
2022-06-20 18:26:53 +0200

    myapp.js

---
+++ myApp.js
@@ -0,0 +1,17 @@
+const mySecret = process.env['MESSAGE_STYLE']
+let express = require('express');
+let app = express();
+
+console.log('Hello World');
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/json", (req, res) => {
+  res.json({
+  message: "Hello json"
+  });
+});
+app.use("/public", express.static(__dirname + "/public"));
+
