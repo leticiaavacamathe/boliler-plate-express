65589cbd4c13ea2c1db83af4d5ffdd084ab12731
leticiaavacamathe <leticia_a.m@hotmail.com>
2022-06-20 18:51:13 +0200

    uppercase

--- myApp.js
+++ myApp.js
@@ -8,11 +8,17 @@ console.log('Hello World');
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/views/index.html");
 });
-
 app.get("/json", (req, res) => {
-    res.json({
-        message: "Hello json"
-    });
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+  res.json({
+    message: "Hello json".toUpperCase()
+  })
+} else {
+   res.json({
+    message: "Hello json"
+  })
+}
+
 });
 app.use("/public", express.static(__dirname + "/public"));
 
