5d3d02bb6b2a8cabc3348fcdf2f79b1b4786b490
leticiaavacamathe <leticia_a.m@hotmail.com>
2022-06-20 18:44:34 +0200

    dotenv

--- myApp.js
+++ myApp.js
@@ -1,17 +1,18 @@
+require('dotenv').config();
 const mySecret = process.env['MESSAGE_STYLE']
 let express = require('express');
 let app = express();
 
 console.log('Hello World');
 
-app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/views/index.html");
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/views/index.html");
 });
 
 app.get("/json", (req, res) => {
-  res.json({
-  message: "Hello json"
-  });
+    res.json({
+        message: "Hello json"
+    });
 });
 app.use("/public", express.static(__dirname + "/public"));
 
